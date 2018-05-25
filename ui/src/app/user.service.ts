1c70507731712427b892d60dc46ed917521b96a6
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-25 16:58:43 -0700

    added envrionment api varibles

--- ui/src/app/user.service.ts
+++ ui/src/app/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,22 @@ export class UserService {
   ) { }
 
   getAllUsers() {
-    return this.http.get('/api/user');
+    return this.http.get(`${environment.apiHost}/api/user`);
   }
 
   getOneUser(userID) {
-    return this.http.get('/api/user/' + userID);
+    return this.http.get(`${environment.apiHost}/api/user/` + userID);
   }
 
   addNewUser(user) {
-    return this.http.post('/api/user', user);
+    return this.http.post(`${environment.apiHost}/api/user`, user);
   }
 
   updateUser(userID, user) {
-    return this.http.put('/api/user/' + userID, user);
+    return this.http.put(`${environment.apiHost}/api/user/` + userID, user);
   }
 
   deleteUser(userID) {
-    return this.http.delete('/api/user/' + userID);
+    return this.http.delete(`${environment.apiHost}/api/user/` + userID);
   }
 }
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHost: 'http://ec2-13-56-82-215.us-west-1.compute.amazonaws.com:8080'
 };
