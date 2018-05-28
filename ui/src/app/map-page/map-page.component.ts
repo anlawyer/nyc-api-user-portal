6ac417edcc1ba1d2248986615575a639a413c0fb
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-28 16:29:07 -0700

    added map, renders with styles

--- ui/src/app/map-page/map-page.component.ts
+++ ui/src/app/map-page/map-page.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPageComponent implements OnInit {
 
+  map: any;
+
   constructor() { }
 
   ngOnInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW5sYXd5ZXIiLCJhIjoiY2pnOXU5dHFpMWhwcjJxcWxseG91NDl5ZSJ9.lyKwIU3Uu_XrqQLcw-hE1g';
+    this.map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/anlawyer/cjhk37km103vb2smzmvqzep7i'
+    });
   }
 
 }
