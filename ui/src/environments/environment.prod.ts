ffe50f8b8ac9f0c40419fe25638775630588f560
Alison Lawyer <anlawyer@berkeley.edu>
2018-06-01 08:37:20 -0700

    updated to new server location

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiHost: 'http://ec2-13-56-82-215.us-west-1.compute.amazonaws.com:8080',
+  apiHost: 'http://ec2-13-57-192-24.us-west-1.compute.amazonaws.com:8080',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYW5sYXd5ZXIiLCJhIjoiY2pnOXU5dHFpMWhwcjJxcWxseG91NDl5ZSJ9.lyKwIU3Uu_XrqQLcw-hE1g'
   }
